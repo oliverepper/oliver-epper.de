74ad9dd3dec9dc4450857d8f71c258f079c44b9d
Oliver Epper <oliver.epper@gmail.com>
2022-06-21 18:04:14 +0200

    killer-robots.txt

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ssh-add -T ~/.ssh/id_rsa.pub &&
-export PYTHON_LIBRARY=/Applications/Xcode-13.3.1.app/Contents/Developer/Library/Frameworks/Python3.framework/Versions/Current/lib/libpython3.8.dylib
+export PYTHON_LIBRARY=/Applications/Xcode-13.4.1.app/Contents/Developer/Library/Frameworks/Python3.framework/Versions/Current/lib/libpython3.8.dylib
 publish generate &&
 rsync -a Output/* oliver@shiny:~/oliver-epper.de/html/ &&
 publish deploy
